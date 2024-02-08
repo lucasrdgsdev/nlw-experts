b8f2d93080d7228f8922d5bb166fc6e7446b2417
lucasrdgsdev <134662314+lucasrdgsdev@users.noreply.github.com>
2024-02-07 22:11:30 -0300

    Teste de Fisioterapia

--- index.js
+++ index.js
@@ -1,95 +1,96 @@
 const perguntas = [
-    {
-      pergunta: "Qual é a forma correta de declarar uma variável em JavaScript?",
-      respostas: [
-        "let myVar = 10;",
-        "const myVar = 10;",
-        "var myVar = 10;",
-      ],
-      correta: 1
-    },
-    {
-      pergunta: "Qual função é usada para imprimir mensagens no console?",
-      respostas: [
-        "print()",
-        "log()",
-        "console.log()",
-      ],
-      correta: 2
-    },
-    {
-      pergunta: "Como você verifica o tipo de uma variável em JavaScript?",
-      respostas: [
-        "typeOf()",
-        "typeof()",
-        "type()",
-      ],
-      correta: 1
-    },
-    {
-      pergunta: "O que é um array em JavaScript?",
-      respostas: [
-        "Uma função",
-        "Um tipo de dado",
-        "Um operador lógico",
-      ],
-      correta: 1
-    },
-    {
-      pergunta: "Como você adiciona um elemento no final de um array?",
-      respostas: [
-        "array.add()",
-        "array.push()",
-        "array.append()",
-      ],
-      correta: 1
-    },
-    {
-      pergunta: "O que é um loop 'for' em JavaScript?",
-      respostas: [
-        "Uma estrutura condicional",
-        "Um tipo de dado",
-        "Uma estrutura de repetição",
-      ],
-      correta: 2
-    },
-    {
-      pergunta: "Qual é a função do operador '===' em JavaScript?",
-      respostas: [
-        "Comparação de valor e tipo",
-        "Atribuição de valor",
-        "Comparação de valor",
-      ],
-      correta: 0
-    },
-    {
-      pergunta: "Como você converte uma string para um número em JavaScript?",
-      respostas: [
-        "parseInt()",
-        "stringToNumber()",
-        "convertToNumber()",
-      ],
-      correta: 0
-    },
-    {
-      pergunta: "O que é o DOM em JavaScript?",
-      respostas: [
-        "Document Object Model",
-        "Data Object Model",
-        "Dynamic Object Model",
-      ],
-      correta: 0
-    },
-    {
-      pergunta: "Qual é a sintaxe correta para uma função em JavaScript?",
-      respostas: [
-        "function myFunction() {}",
-        "myFunction: function() {}",
-        "const myFunction = () => {}",
-      ],
-      correta: 0
-    },
-  ];
+  {
+    pergunta: "Qual é o principal objetivo da fisioterapia motora?",
+    respostas: [
+      "Aumentar a capacidade pulmonar",
+      "Restaurar a função motora",
+      "Melhorar a visão"
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "Qual técnica é comumente utilizada na fisioterapia para melhorar a amplitude de movimento?",
+    respostas: [
+      "Massagem cardíaca",
+      "Exercícios de fortalecimento",
+      "Acupuntura"
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Em que condição clínica a fisioterapia motora pode ser aplicada?",
+    respostas: [
+      "Insônia",
+      "Doença cardiovascular",
+      "Alergias sazonais"
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "Quais são os benefícios da fisioterapia motora para pacientes com lesões neuromusculares?",
+    respostas: [
+      "Diminuição da mobilidade",
+      "Aumento da dor",
+      "Melhora na coordenação motora"
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "O que é a neuroplasticidade no contexto da fisioterapia motora?",
+    respostas: [
+      "Uma técnica de massagem",
+      "A capacidade do cérebro de se adaptar e remodelar",
+      "Um tipo de exercício cardiovascular"
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "Quais atividades podem ser incluídas em um programa de fisioterapia motora para idosos?",
+    respostas: [
+      "Aulas de culinária",
+      "Exercícios de equilíbrio e marcha",
+      "Assistir televisão"
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "Qual é a importância da avaliação inicial na fisioterapia motora?",
+    respostas: [
+      "Não é relevante",
+      "Ajuda a identificar as necessidades do paciente",
+      "Atrasa o processo de recuperação"
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "Quando a fisioterapia motora é frequentemente indicada após uma cirurgia?",
+    respostas: [
+      "Nunca é indicada",
+      "Sempre é indicada",
+      "Depende do tipo de cirurgia e das necessidades do paciente"
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "O que são exercícios isométricos na fisioterapia motora?",
+    respostas: [
+      "Exercícios que envolvem movimentos rápidos",
+      "Exercícios que envolvem contração muscular sem movimento",
+      "Exercícios de respiração profunda"
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "Quais são os sinais de alerta durante uma sessão de fisioterapia motora?",
+    respostas: [
+      "Desconforto leve",
+      "Dor persistente e aumento da dor",
+      "Fadiga moderada"
+    ],
+    correta: 2
+  }
+];
+
   
   const quiz = document.querySelector('#quiz')
   const template = document.querySelector('template')
